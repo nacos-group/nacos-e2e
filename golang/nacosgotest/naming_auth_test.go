d731d01280a9c79848dd78bc5afb868ab1beb731
kiko-cyy <caoyinyin_cyy@163.com>
2023-06-30 11:44:17 +0800

    open the go sdk case

---
+++ golang/nacosgotest/naming_auth_test.go
@@ -0,0 +1,342 @@
+package nacos_go_test
+
+import (
+	"fmt"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+	"github.com/stretchr/testify/assert"
+	. "nacos_go_test/utils"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+var AuthTrueNamingClient = CreateNamingClient(false)
+var AuthFailNamingClient = CreateAuthFailNamingClient(false)
+var NoAuthNamingClient = CreateNoAuthNamingClient(false)
+
+func Test_RegisterInstance_AuthFail(t *testing.T) {
+	t.Run("TestErrorAKSK", func(t *testing.T) {
+		var serviceName string = RandServiceName(10)
+		success, err := AuthFailNamingClient.RegisterInstance(vo.RegisterInstanceParam{
+			ServiceName: serviceName,
+			Ip:          TEST_IP_1,
+			Port:        TEST_PORT_8848,
+			Ephemeral:   false,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.NotNil(t, err)
+		assert.True(t, strings.Contains(string(err.Error()), "403"))
+		assert.Equal(t, false, success)
+	})
+	t.Run("TestNoAKSK", func(t *testing.T) {
+		var serviceName string = RandServiceName(10)
+		success, err := NoAuthNamingClient.RegisterInstance(vo.RegisterInstanceParam{
+			ServiceName: serviceName,
+			Ip:          TEST_IP_1,
+			Port:        TEST_PORT_8848,
+			Ephemeral:   false,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.NotNil(t, err)
+		assert.True(t, strings.Contains(string(err.Error()), "403"))
+		assert.Equal(t, false, success)
+	})
+}
+
+func Test_BatchRegisterInstance_AuthFail(t *testing.T) {
+	t.Run("TestErrorAKSK", func(t *testing.T) {
+		var serviceName string = RandServiceName(10)
+		var instances []vo.RegisterInstanceParam
+		instance1 := vo.RegisterInstanceParam{
+			Ip:        "127.0.0.1",
+			Port:      8080,
+			Weight:    1,
+			Enable:    true,
+			Healthy:   true,
+			Ephemeral: true,
+		}
+		instance2 := vo.RegisterInstanceParam{
+			Ip:        "127.0.0.1",
+			Port:      8081,
+			Weight:    1,
+			Enable:    true,
+			Healthy:   true,
+			Ephemeral: true,
+		}
+		instances = append(instances, instance1)
+		instances = append(instances, instance2)
+
+		success, err := AuthFailNamingClient.BatchRegisterInstance(vo.BatchRegisterInstanceParam{
+			ServiceName: serviceName,
+			GroupName:   DEFAULT_GROUP,
+			Instances:   instances,
+		})
+		assert.Equal(t, false, success)
+		if !success {
+			assert.True(t, err != nil)
+			assert.True(t, strings.Contains(string(err.Error()), "403"))
+		}
+	})
+
+	t.Run("TestNoAKSK", func(t *testing.T) {
+		var serviceName string = RandServiceName(10)
+		var instances []vo.RegisterInstanceParam
+		instance1 := vo.RegisterInstanceParam{
+			Ip:        "127.0.0.1",
+			Port:      8080,
+			Weight:    1,
+			Enable:    true,
+			Healthy:   true,
+			Ephemeral: true,
+		}
+		instance2 := vo.RegisterInstanceParam{
+			Ip:        "127.0.0.1",
+			Port:      8081,
+			Weight:    1,
+			Enable:    true,
+			Healthy:   true,
+			Ephemeral: true,
+		}
+		instances = append(instances, instance1)
+		instances = append(instances, instance2)
+
+		success, err := NoAuthNamingClient.BatchRegisterInstance(vo.BatchRegisterInstanceParam{
+			ServiceName: serviceName,
+			GroupName:   DEFAULT_GROUP,
+			Instances:   instances,
+		})
+
+		assert.Equal(t, false, success)
+		if !success {
+			assert.True(t, err != nil)
+			assert.True(t, strings.Contains(string(err.Error()), "403"))
+		}
+	})
+}
+
+func registerInstance(serviceName string, ip string, port uint64) bool {
+	success, err := AuthTrueNamingClient.RegisterInstance(vo.RegisterInstanceParam{
+		ServiceName: serviceName,
+		Ip:          ip,
+		Port:        port,
+		Ephemeral:   true,
+	})
+	fmt.Printf("RegisterInstance: success %s, err %s\n", strconv.FormatBool(success), err)
+	return success
+}
+
+func registerInstance1(serviceName string, ip string, port uint64, ephemeral bool) bool {
+	success, err := AuthTrueNamingClient.RegisterInstance(vo.RegisterInstanceParam{
+		ServiceName: serviceName,
+		Ip:          ip,
+		Port:        port,
+		Ephemeral:   ephemeral,
+	})
+	fmt.Printf("RegisterInstance: success %s, err %s\n", strconv.FormatBool(success), err)
+	return success
+}
+
+func deregisterInstance(serviceName string, ip string, port uint64, groupName string) bool {
+	success, err := AuthTrueNamingClient.DeregisterInstance(vo.DeregisterInstanceParam{
+		ServiceName: serviceName,
+		Ip:          ip,
+		Port:        port,
+		Ephemeral:   true,
+		GroupName:   groupName,
+	})
+	fmt.Printf("DeregisterInstance: success %s, err %s\n", strconv.FormatBool(success), err)
+	return success
+}
+
+func Test_DeregisterInstance_AuthFail(t *testing.T) {
+	t.Run("TestErrorAKSK", func(t *testing.T) {
+		var serviceName string = RandServiceName(10)
+		successR := registerInstance(serviceName, TEST_IP_1, TEST_PORT_8848)
+		assert.True(t, successR)
+		success, err := AuthFailNamingClient.DeregisterInstance(vo.DeregisterInstanceParam{
+			ServiceName: serviceName,
+			Ip:          TEST_IP_1,
+			Port:        TEST_PORT_8848,
+			Ephemeral:   true,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.Equal(t, false, success)
+		if !success && nil != err {
+			assert.NotNil(t, err)
+			assert.True(t, strings.Contains(string(err.Error()), "403"))
+		}
+		successD := deregisterInstance(serviceName, TEST_IP_1, TEST_PORT_8848, DEFAULT_GROUP)
+		assert.True(t, successD)
+
+	})
+	t.Run("TestNoAKSK", func(t *testing.T) {
+		var serviceName string = RandServiceName(10)
+		successR := registerInstance(serviceName, TEST_IP_1, TEST_PORT_8848)
+		assert.True(t, successR)
+		success, err := NoAuthNamingClient.DeregisterInstance(vo.DeregisterInstanceParam{
+			ServiceName: serviceName,
+			Ip:          TEST_IP_1,
+			Port:        TEST_PORT_8848,
+			Ephemeral:   true,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.Equal(t, false, success)
+		if !success && nil != err {
+			assert.NotNil(t, err)
+			assert.True(t, strings.Contains(string(err.Error()), "403"))
+		}
+		successD := deregisterInstance(serviceName, TEST_IP_1, TEST_PORT_8848, DEFAULT_GROUP)
+		assert.True(t, successD)
+	})
+}
+
+func Test_UpdateInstance_AuthFail(t *testing.T) {
+	t.Run("TestErrorAKSK", func(t *testing.T) {
+		var serviceName string = RandServiceName(10)
+		successR := registerInstance(serviceName, TEST_IP_1, TEST_PORT_8848)
+		assert.True(t, successR)
+		success, err := AuthFailNamingClient.UpdateInstance(vo.UpdateInstanceParam{
+			ServiceName: serviceName,
+			Ip:          TEST_IP_1,
+			Port:        TEST_PORT_8848,
+			Ephemeral:   false,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.NotNil(t, err)
+		assert.True(t, strings.Contains(string(err.Error()), "403"))
+		assert.Equal(t, false, success)
+		successD := deregisterInstance(serviceName, TEST_IP_1, TEST_PORT_8848, DEFAULT_GROUP)
+		assert.True(t, successD)
+	})
+	t.Run("TestNoAKSK", func(t *testing.T) {
+		var serviceName string = RandServiceName(10)
+		successR := registerInstance(serviceName, TEST_IP_1, TEST_PORT_8848)
+		assert.True(t, successR)
+		success, err := NoAuthNamingClient.UpdateInstance(vo.UpdateInstanceParam{
+			ServiceName: serviceName,
+			Ip:          TEST_IP_1,
+			Port:        TEST_PORT_8848,
+			Ephemeral:   false,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.NotNil(t, err)
+		assert.True(t, strings.Contains(string(err.Error()), "403"))
+		assert.Equal(t, false, success)
+		successD := deregisterInstance(serviceName, TEST_IP_1, TEST_PORT_8848, DEFAULT_GROUP)
+		assert.True(t, successD)
+	})
+}
+
+func Test_SearchInterface_AuthFail(t *testing.T) {
+	t.Run("TestErrorAKSK", func(t *testing.T) {
+		var serviceName string = RandServiceName(10)
+		successR := registerInstance1(serviceName, TEST_IP_1, TEST_PORT_8848, false)
+		assert.True(t, successR)
+
+		result, err := AuthTrueNamingClient.GetService(vo.GetServiceParam{
+			ServiceName: serviceName,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.True(t, result.Name == serviceName)
+		assert.True(t, result.GroupName == DEFAULT_GROUP)
+
+		result, err = AuthFailNamingClient.GetService(vo.GetServiceParam{
+			ServiceName: serviceName,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.True(t, result.Name == "")
+		assert.True(t, result.GroupName == "")
+		//assert.Equal(t, nil, result)
+		//assert.NotNil(t, err)
+		//assert.True(t, strings.Contains(string(err.Error()), "403"))
+
+		values, errs := AuthTrueNamingClient.SelectAllInstances(vo.SelectAllInstancesParam{
+			ServiceName: serviceName,
+			GroupName:   DEFAULT_GROUP,
+		})
+
+		values, errs = AuthFailNamingClient.SelectAllInstances(vo.SelectAllInstancesParam{
+			ServiceName: serviceName,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.Equal(t, nil, values)
+		assert.NotNil(t, errs)
+		assert.True(t, strings.Contains(string(errs.Error()), "403"))
+
+		results, err := AuthFailNamingClient.SelectInstances(vo.SelectInstancesParam{
+			ServiceName: serviceName,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.True(t, len(results) == 0)
+		assert.NotNil(t, err)
+		assert.True(t, strings.Contains(string(err.Error()), "403"))
+
+		res, err := AuthFailNamingClient.SelectOneHealthyInstance(vo.SelectOneHealthInstanceParam{
+			ServiceName: serviceName,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.Nil(t, res)
+		assert.True(t, strings.Contains(string(err.Error()), "403"))
+
+		allServices, errs := AuthFailNamingClient.GetAllServicesInfo(vo.GetAllServiceInfoParam{
+			NameSpace: Ns,
+			GroupName: DEFAULT_GROUP,
+			PageNo:    1,
+			PageSize:  100,
+		})
+		assert.Nil(t, allServices.Doms)
+		assert.True(t, allServices.Count == 0)
+		assert.True(t, strings.Contains(string(errs.Error()), "403"))
+
+		successD := deregisterInstance(serviceName, TEST_IP_1, TEST_PORT_8848, DEFAULT_GROUP)
+		assert.True(t, successD)
+	})
+	t.Run("TestNoAKSK", func(t *testing.T) {
+		var serviceName string = RandServiceName(10)
+		successR := registerInstance(serviceName, TEST_IP_1, TEST_PORT_8848)
+		assert.True(t, successR)
+		result, err := NoAuthNamingClient.GetService(vo.GetServiceParam{
+			ServiceName: serviceName,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.Equal(t, nil, result)
+		assert.NotNil(t, err)
+		assert.True(t, strings.Contains(string(err.Error()), "403"))
+
+		values, errs := NoAuthNamingClient.SelectAllInstances(vo.SelectAllInstancesParam{
+			ServiceName: serviceName,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.Equal(t, nil, values)
+		assert.NotNil(t, errs)
+		assert.True(t, strings.Contains(string(errs.Error()), "403"))
+
+		results, err := NoAuthNamingClient.SelectInstances(vo.SelectInstancesParam{
+			ServiceName: serviceName,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.True(t, len(results) == 0)
+		assert.NotNil(t, err)
+		assert.True(t, strings.Contains(string(err.Error()), "403"))
+
+		res, err := NoAuthNamingClient.SelectOneHealthyInstance(vo.SelectOneHealthInstanceParam{
+			ServiceName: serviceName,
+			GroupName:   DEFAULT_GROUP,
+		})
+		assert.Nil(t, res)
+		assert.True(t, strings.Contains(string(err.Error()), "403"))
+
+		allServices, errs := NoAuthNamingClient.GetAllServicesInfo(vo.GetAllServiceInfoParam{
+			NameSpace: Ns,
+			GroupName: DEFAULT_GROUP,
+			PageNo:    1,
+			PageSize:  100,
+		})
+		assert.Nil(t, allServices.Doms)
+		assert.True(t, allServices.Count == 0)
+		assert.True(t, strings.Contains(string(errs.Error()), "403"))
+
+		successD := deregisterInstance(serviceName, TEST_IP_1, TEST_PORT_8848, DEFAULT_GROUP)
+		assert.True(t, successD)
+	})
+}
---
+++ golang/util/nacos_base.go
@@ -0,0 +1,401 @@
+package utils
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients/config_client"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients/naming_client"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/util"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var serverList string = "127.0.0.1"
+var curServer = ""
+var Ns = ""
+var Ak = ""
+var Sk = ""
+
+const DEFAULT_GROUP = "DEFAULT_GROUP"
+const TEST_IP_1 = "127.0.0.1"
+const TEST_IP_2 = "127.0.0.2"
+const TEST_PORT_8080 = 8080
+const TEST_PORT_8848 = 8848
+
+func init() {
+	serverList = os.Getenv("serverList")
+	Ns = os.Getenv("namespace")
+	Ak = os.Getenv("ACCESS_KEY")
+	Sk = os.Getenv("SECRET_KEY")
+
+//  If debugging locally, open below and configure env param
+// 	serverList = "127.0.0.1"
+// 	Ns = ""
+// 	Ak = "XXXXX"
+// 	Sk = "XXXXX"
+
+	curServer = "http://" + serverList + ":8848"
+	fmt.Printf("init: serverList %s, curServer %s, ns %s, Ak %s, Sk %s\n", serverList, curServer, Ns, Ak, Sk)
+}
+
+func CreateConfigClient() config_client.IConfigClient {
+	var clientConfig = *constant.NewClientConfig(
+		constant.WithNamespaceId(Ns),
+		constant.WithAccessKey(Ak),
+		constant.WithSecretKey(Sk),
+		constant.WithTimeoutMs(5000),
+		constant.WithNotLoadCacheAtStart(true),
+		constant.WithLogDir("/tmp/nacos/log"),
+		constant.WithCacheDir("/tmp/nacos/cache"),
+		constant.WithLogLevel("debug"),
+	)
+	var serverConfig = []constant.ServerConfig{*constant.NewServerConfig(serverList, 8848, constant.WithContextPath("/nacos"))}
+
+	client, err := clients.NewConfigClient(
+		vo.NacosClientParam{
+			ClientConfig:  &clientConfig,
+			ServerConfigs: serverConfig,
+		},
+	)
+
+	if err != nil {
+		panic("NewConfigClient failed!")
+	}
+	return client
+}
+
+func CreateAuthFailConfigClient() config_client.IConfigClient {
+	var clientConfig = *constant.NewClientConfig(
+		constant.WithNamespaceId(Ns),
+		constant.WithAccessKey("abcdefd"),
+		constant.WithSecretKey("hijklmn"),
+		constant.WithTimeoutMs(5000),
+		constant.WithNotLoadCacheAtStart(true),
+		constant.WithLogDir("/tmp/nacos/log"),
+		constant.WithCacheDir("/tmp/nacos/cache"),
+		constant.WithLogLevel("debug"),
+	)
+	var serverConfig = []constant.ServerConfig{*constant.NewServerConfig(serverList, 8848, constant.WithContextPath("/nacos"))}
+
+	client, err := clients.NewConfigClient(
+		vo.NacosClientParam{
+			ClientConfig:  &clientConfig,
+			ServerConfigs: serverConfig,
+		},
+	)
+
+	if err != nil {
+		panic("NewConfigClient failed!")
+	}
+	return client
+}
+
+func CreateNoAuthConfigClient() config_client.IConfigClient {
+	var clientConfig = *constant.NewClientConfig(
+		constant.WithNamespaceId(Ns),
+		constant.WithTimeoutMs(5000),
+		constant.WithNotLoadCacheAtStart(true),
+		constant.WithLogDir("/tmp/nacos/log"),
+		constant.WithCacheDir("/tmp/nacos/cache"),
+		constant.WithLogLevel("debug"),
+	)
+	var serverConfig = []constant.ServerConfig{*constant.NewServerConfig(serverList, 8848, constant.WithContextPath("/nacos"))}
+
+	client, err := clients.NewConfigClient(
+		vo.NacosClientParam{
+			ClientConfig:  &clientConfig,
+			ServerConfigs: serverConfig,
+		},
+	)
+
+	if err != nil {
+		panic("NewConfigClient failed!")
+	}
+	return client
+}
+
+func CreateNamingClient(updateCacheWhenEmpty bool) naming_client.INamingClient {
+	var clientConfig = *constant.NewClientConfig(
+		constant.WithNamespaceId(Ns),
+		constant.WithAccessKey(Ak),
+		constant.WithSecretKey(Sk),
+		constant.WithUpdateCacheWhenEmpty(updateCacheWhenEmpty), // false protect, true unProtect
+		constant.WithTimeoutMs(5000),
+		constant.WithNotLoadCacheAtStart(true),
+		constant.WithLogDir("/tmp/nacos/log"),
+		constant.WithCacheDir("/tmp/nacos/cache"),
+		constant.WithLogLevel("debug"),
+	)
+	var serverConfig = []constant.ServerConfig{*constant.NewServerConfig(serverList, 8848, constant.WithContextPath("/nacos"))}
+
+	var clientParam = vo.NacosClientParam{
+		ClientConfig:  &clientConfig,
+		ServerConfigs: serverConfig,
+	}
+
+	client, err := clients.NewNamingClient(clientParam)
+	if err != nil {
+		panic("NewNamingClient failed!")
+	}
+	return client
+}
+
+func CreateAuthFailNamingClient(updateCacheWhenEmpty bool) naming_client.INamingClient {
+	var clientConfig = *constant.NewClientConfig(
+		constant.WithNamespaceId(Ns),
+		constant.WithAccessKey("abcdefd"),
+		constant.WithSecretKey("hijklmn"),
+		constant.WithUpdateCacheWhenEmpty(updateCacheWhenEmpty), // false protect, true unProtect
+		constant.WithTimeoutMs(5000),
+		constant.WithNotLoadCacheAtStart(true),
+		constant.WithLogDir("/tmp/nacos/log"),
+		constant.WithCacheDir("/tmp/nacos/cache"),
+		constant.WithLogLevel("debug"),
+	)
+	var serverConfig = []constant.ServerConfig{*constant.NewServerConfig(serverList, 8848, constant.WithContextPath("/nacos"))}
+
+	var clientParam = vo.NacosClientParam{
+		ClientConfig:  &clientConfig,
+		ServerConfigs: serverConfig,
+	}
+
+	client, err := clients.NewNamingClient(clientParam)
+	if err != nil {
+		panic("NewNamingClient failed!")
+	}
+	return client
+}
+
+func CreateNoAuthNamingClient(updateCacheWhenEmpty bool) naming_client.INamingClient {
+	var clientConfig = *constant.NewClientConfig(
+		constant.WithNamespaceId(Ns),
+		constant.WithUpdateCacheWhenEmpty(updateCacheWhenEmpty), // false protect, true unProtect
+		constant.WithTimeoutMs(5000),
+		constant.WithNotLoadCacheAtStart(true),
+		constant.WithLogDir("/tmp/nacos/log"),
+		constant.WithCacheDir("/tmp/nacos/cache"),
+		constant.WithLogLevel("debug"),
+	)
+	var serverConfig = []constant.ServerConfig{*constant.NewServerConfig(serverList, 8848, constant.WithContextPath("/nacos"))}
+
+	var clientParam = vo.NacosClientParam{
+		ClientConfig:  &clientConfig,
+		ServerConfigs: serverConfig,
+	}
+
+	client, err := clients.NewNamingClient(clientParam)
+	if err != nil {
+		panic("NewNamingClient failed!")
+	}
+	return client
+}
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func RandStr(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func RandDataId(n int) string {
+	return "gotest." + RandStr(n)
+}
+
+func RandCipherDataId(n int) string {
+	return "cipher-gotest." + RandStr(n)
+}
+
+func RandServiceName(n int) string {
+	return "gotest." + RandStr(n)
+}
+
+func HttpGet(path string, params map[string]string) string {
+	client := &http.Client{}
+
+	reqUrl := curServer + path
+	fmt.Println("HttpGet reqUrl:", reqUrl)
+
+	timestamp := fmt.Sprint(time.Now().UnixNano() / 1e6)
+	signature := signWithhmacSHA1Encrypt(timestamp, Sk)
+	params["Timestamp"] = timestamp
+	params["Spas-AccessKey"] = Ak
+	params["Spas-Signature"] = signature
+
+	reqUrl += "?" + encodeParams(params)
+	fmt.Println("HttpGet newReqUrl:", reqUrl)
+
+	req, err := http.NewRequest("GET", reqUrl, nil)
+	if err != nil {
+		fmt.Println("Error creating HTTP request:", err)
+		return ""
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending HTTP request:", err)
+		return ""
+	}
+
+	if resp.StatusCode != 200 {
+		fmt.Println("Error response:", resp.Status)
+		return ""
+	}
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading HTTP response:", err)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("HttpGet body:", string(responseBody))
+	return string(responseBody)
+}
+
+func HttpPost(path string, params map[string]string) string {
+	client := &http.Client{}
+
+	reqUrl := curServer + path
+	fmt.Println("HttpPost reqUrl:", reqUrl)
+
+	signData := getNamingSignData(params)
+	params["signature"] = signWithhmacSHA1Encrypt(signData, Sk)
+	params["ak"] = Ak
+	params["data"] = signData
+
+	reqUrl += "?" + encodeParams(params)
+	fmt.Println("HttpPost newReqUrl:", reqUrl)
+
+	body := util.GetUrlFormedMap(params)
+	req, err := http.NewRequest("POST", reqUrl, strings.NewReader(body))
+	if err != nil {
+		fmt.Println("Error creating HTTP request:", err)
+		return ""
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending HTTP request:", err)
+		return ""
+	}
+
+	if resp.StatusCode != 200 {
+		fmt.Println("Error response:", resp.Status)
+		return ""
+	}
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading HTTP response:", err)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("HttpPost body:", string(responseBody))
+	return string(responseBody)
+}
+
+func addSignParam(signType string, params map[string]string) map[string]string {
+	if "config" == signType {
+		timestamp := fmt.Sprint(time.Now().UnixNano() / 1e6)
+
+		if group, hasGroup := params["group"]; hasGroup {
+			signData := getConfigSignData(timestamp, Ns, group)
+			params["Spas-Signature"] = signWithhmacSHA1Encrypt(signData, Sk)
+			params["Timestamp"] = timestamp
+			params["Spas-AccessKey"] = Ak
+		} else {
+			signature := signWithhmacSHA1Encrypt(timestamp, Sk)
+			params["Timestamp"] = timestamp
+			params["Spas-AccessKey"] = Ak
+			params["Spas-Signature"] = signature
+		}
+	}
+
+	if "naming" == signType {
+		signData := getNamingSignData(params)
+		params["signature"] = signWithhmacSHA1Encrypt(signData, Sk)
+		params["ak"] = Ak
+		params["data"] = signData
+	}
+	return params
+}
+
+func getNamingSignData(param map[string]string) string {
+	var signData string
+	timeStamp := strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
+	if serviceName, hasServiceName := param["serviceName"]; hasServiceName {
+		if groupName, hasGroup := param["groupName"]; strings.Contains(serviceName, constant.SERVICE_INFO_SPLITER) || !hasGroup || groupName == "" {
+			signData = timeStamp + constant.SERVICE_INFO_SPLITER + serviceName
+		} else {
+			signData = timeStamp + constant.SERVICE_INFO_SPLITER + util.GetGroupName(serviceName, groupName)
+		}
+	} else {
+		signData = timeStamp
+	}
+	return signData
+}
+
+func getConfigSignData(timeStamp string, ns string, group string) string {
+	var signData string
+
+	resource := ""
+	if len(ns) != 0 {
+		resource = ns + group
+	} else {
+		resource = group
+	}
+
+	if resource == "" {
+		signData = timeStamp
+	} else {
+		signData = resource + "+" + timeStamp
+	}
+	return signData
+}
+
+func signWithhmacSHA1Encrypt(encryptText, encryptKey string) string {
+	//hmac ,use sha1
+	key := []byte(encryptKey)
+	mac := hmac.New(sha1.New, key)
+	mac.Write([]byte(encryptText))
+
+	return base64.StdEncoding.EncodeToString(mac.Sum(nil))
+}
+
+func encodeParams(params map[string]string) string {
+	reqParams := url.Values{}
+	for key, val := range params {
+		reqParams.Set(key, val)
+	}
+	return reqParams.Encode()
+}
+
+func Contains(arr []string, str string) bool {
+	for _, v := range arr {
+		if strings.Contains(v, str) {
+			return true
+		}
+	}
+	return false
+}
+
+func ToJsonString(object interface{}) string {
+	js, _ := json.Marshal(object)
+	return string(js)
+}
